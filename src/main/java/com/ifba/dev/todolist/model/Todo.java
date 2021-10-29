a1cd4a567d92f31e063d0e65e1c206a0e4e8dd90
Itor-Carlos <itor51carlos@gmail.com>
2021-10-29 20:49:03 -0300

    adicionado getters e setters da classe Todo

--- src/main/java/com/ifba/dev/todolist/model/Todo.java
+++ src/main/java/com/ifba/dev/todolist/model/Todo.java
@@ -14,4 +14,27 @@ public class Todo {
         this.descricao = descricao;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
