c6aca183f612ee63d9dde209b95a30e4a1a21afd
Itor-Carlos <itor51carlos@gmail.com>
2021-10-29 21:19:16 -0300

    mapeado metodo listar da classe TodoController com GetMapping

--- src/main/java/com/ifba/dev/todolist/controllers/TodoController.java
+++ src/main/java/com/ifba/dev/todolist/controllers/TodoController.java
@@ -4,6 +4,7 @@ import com.ifba.dev.todolist.model.Todo;
 import com.ifba.dev.todolist.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class TodoController {
     @Autowired
     private TodoService todoService;
 
+    @GetMapping
     public ResponseEntity<List<Todo>> listar(){
         List<Todo> listaGeral = this.todoService.getAll();
         return ResponseEntity.ok(listaGeral);
