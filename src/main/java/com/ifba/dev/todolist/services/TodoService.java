6716a885f1382626e4c21b583ad66ac4f2c0a97c
Itor-Carlos <itor51carlos@gmail.com>
2021-10-29 21:16:19 -0300

    criado e implementado metodo getAll na classe TodoService

--- src/main/java/com/ifba/dev/todolist/services/TodoService.java
+++ src/main/java/com/ifba/dev/todolist/services/TodoService.java
@@ -1,13 +1,22 @@
 package com.ifba.dev.todolist.services;
 
+import com.ifba.dev.todolist.model.Todo;
 import com.ifba.dev.todolist.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 public class TodoService {
 
     @Autowired
     private TodoRepository todoRepository;
+
+    public List<Todo> getAll(){
+        return this.todoRepository.findAll();
+    }
+
 
 }
