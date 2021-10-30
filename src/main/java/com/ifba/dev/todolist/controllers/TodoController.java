a830684adf03bff0e30da66d56e4892b94c1d3f0
Itor-Carlos <itor51carlos@gmail.com>
2021-10-29 21:05:36 -0300

    adicionado atributo do tipo TodoService na classe TodoController

--- src/main/java/com/ifba/dev/todolist/controllers/TodoController.java
+++ src/main/java/com/ifba/dev/todolist/controllers/TodoController.java
@@ -1,5 +1,7 @@
 package com.ifba.dev.todolist.controllers;
 
+import com.ifba.dev.todolist.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/todos")
 public class TodoController {
 
+    @Autowired
+    private TodoService todoService;
+    
 
 }
