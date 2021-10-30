3342e091cc801d4024387c7905eaa5fa822c64f3
Itor-Carlos <itor51carlos@gmail.com>
2021-10-29 21:04:25 -0300

    mapeada classe TodoController com RestController e RequestMapping

--- src/main/java/com/ifba/dev/todolist/controllers/TodoController.java
+++ src/main/java/com/ifba/dev/todolist/controllers/TodoController.java
@@ -1,6 +1,11 @@
 package com.ifba.dev.todolist.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/todos")
 public class TodoController {
 
-    
+
 }
