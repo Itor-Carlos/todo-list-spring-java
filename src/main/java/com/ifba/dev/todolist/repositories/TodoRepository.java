548e8603abde734686c2c98c8d2667bd03a5ca60
Itor-Carlos <itor51carlos@gmail.com>
2021-10-30 09:46:04 -0300

    adicionado metodo searchForId na classe TodoRepository com anotacao @Query

--- src/main/java/com/ifba/dev/todolist/repositories/TodoRepository.java
+++ src/main/java/com/ifba/dev/todolist/repositories/TodoRepository.java
@@ -2,8 +2,16 @@ package com.ifba.dev.todolist.repositories;
 
 import com.ifba.dev.todolist.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo,Long> {
+
+    @Query("select t FROM Todo t WHERE t.id=:id")
+    Todo searchForId(@Param("id") Long id);
+
+
+
 }
