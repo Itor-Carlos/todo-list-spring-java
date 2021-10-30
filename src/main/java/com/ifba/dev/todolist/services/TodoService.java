d1b3fc7d7fd5f4f333990f5a43853f01c1e0da85
Itor-Carlos <itor51carlos@gmail.com>
2021-10-29 21:43:27 -0300

    criado e implementado metodo buscar na classe TodoService

--- src/main/java/com/ifba/dev/todolist/services/TodoService.java
+++ src/main/java/com/ifba/dev/todolist/services/TodoService.java
@@ -1,5 +1,6 @@
 package com.ifba.dev.todolist.services;
 
+import com.ifba.dev.todolist.exceptions.EntityNotFoundException;
 import com.ifba.dev.todolist.model.Todo;
 import com.ifba.dev.todolist.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,15 @@ public class TodoService {
     public List<Todo> getAll(){
         return this.todoRepository.findAll();
     }
+
+    public Todo buscar(Long id){
+        Todo todo = this.todoRepository.getById(id);
+        if(todo == null){
+            throw new EntityNotFoundException("não foi encontrado nenhum Todo com o id passado");
+        }
+        return todo;
+    }
+
 
 
 }
