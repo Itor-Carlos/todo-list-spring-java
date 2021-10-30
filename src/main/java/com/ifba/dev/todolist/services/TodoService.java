bfd7ceb1b4be1c641d39d8792c96f76cc3630404
Itor-Carlos <itor51carlos@gmail.com>
2021-10-29 21:01:58 -0300

    adicionado atributo todoRepository na classe TodoService

--- src/main/java/com/ifba/dev/todolist/services/TodoService.java
+++ src/main/java/com/ifba/dev/todolist/services/TodoService.java
@@ -1,7 +1,13 @@
 package com.ifba.dev.todolist.services;
 
+import com.ifba.dev.todolist.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
 }
