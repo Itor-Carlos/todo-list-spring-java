a22d0d1304841c31260f39653aeca16eb7098063
Itor-Carlos <itor51carlos@gmail.com>
2021-11-13 10:31:27 -0300

    declarado metodo find na interface TodoRepository

--- src/main/java/com/ifba/dev/todolist/repositories/interfaces/TodoRepository.java
+++ src/main/java/com/ifba/dev/todolist/repositories/interfaces/TodoRepository.java
@@ -14,7 +14,7 @@ public interface TodoRepository extends JpaRepository<Todo,Long> {
     @Query("select t FROM Todo t WHERE t.id=:id")
     Todo searchForId(@Param("id") Long id);
 
-
+    public List<Todo> find(Long id, String name, String descricao);
 
 
 }
