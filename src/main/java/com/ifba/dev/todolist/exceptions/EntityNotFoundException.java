e3badc171179b2b2988125652e7272ec837b0c37
Itor-Carlos <itor51carlos@gmail.com>
2022-01-03 16:09:57 -0300

    adicionado construtor vazio na classe EntityNotFoundException

--- src/main/java/com/ifba/dev/todolist/exceptions/EntityNotFoundException.java
+++ src/main/java/com/ifba/dev/todolist/exceptions/EntityNotFoundException.java
@@ -2,6 +2,9 @@ package com.ifba.dev.todolist.exceptions;
 
 public class EntityNotFoundException extends RuntimeException{
 
+    public EntityNotFoundException() {
+    }
+
     public  EntityNotFoundException(String mensagem){
         super(mensagem);
     }
