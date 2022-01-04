217cf83d10b14a7a3ff9f0a7d5ddf098a6b0896a
Itor-Carlos <itor51carlos@gmail.com>
2022-01-03 21:09:03 -0300

    criado metodo de nome todoNotFound e com a anotação @ExceptionHandler para tratamento de exceções do tipo EntityNotFoundException

--- src/main/java/com/ifba/dev/todolist/handler/RestExceptionHandler.java
+++ src/main/java/com/ifba/dev/todolist/handler/RestExceptionHandler.java
@@ -1,10 +1,28 @@
 package com.ifba.dev.todolist.handler;
 
 
+import com.ifba.dev.todolist.exceptions.EntityNotFoundException;
+import com.ifba.dev.todolist.exceptions.EntityNotFoundExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 
 @RestControllerAdvice
 public class RestExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<EntityNotFoundExceptionDetails> todoNotFound(EntityNotFoundException entityNotFoundException){
+        EntityNotFoundExceptionDetails todoNotFound = new EntityNotFoundExceptionDetails(
+                "Todo not found in this id",
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now(),
+                "Entity Not Found"
+        );
+        return new ResponseEntity<>(todoNotFound,HttpStatus.NOT_FOUND);
+    }
 
 }
