a4becee0253dd2d7a99bb02c0b64ade59f61401d
Itor-Carlos <itor51carlos@gmail.com>
2022-01-03 21:03:23 -0300

    criada classe de nome RestExceptionHandler e com anotação @RestControllerAdvice para tratamento de exceções

---
+++ src/main/java/com/ifba/dev/todolist/handler/RestExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.ifba.dev.todolist.handler;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+}
