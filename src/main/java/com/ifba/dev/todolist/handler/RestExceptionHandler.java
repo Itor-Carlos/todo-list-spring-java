177cdb486ee4ffe75b94d105461a8fd4f8c61e49
Itor-Carlos <itor51carlos@gmail.com>
2022-01-06 12:05:31 -0300

    criado metodo de nome methodArgumentException na classe RestExceptionHandler para lidar com exceções do tipo MethodArgumentNotValidException, onde seria retornado um objeto ResposeEntity com um objeto MethodArgumentNotValidExceptionDetails no body

--- src/main/java/com/ifba/dev/todolist/handler/RestExceptionHandler.java
+++ src/main/java/com/ifba/dev/todolist/handler/RestExceptionHandler.java
@@ -4,12 +4,17 @@ package com.ifba.dev.todolist.handler;
 import com.ifba.dev.todolist.exceptions.EntityNotFoundException;
 import com.ifba.dev.todolist.exceptions.EntityNotFoundExceptionDetails;
 import com.ifba.dev.todolist.exceptions.IllegalArgumentExceptionDetails;
+import com.ifba.dev.todolist.exceptions.MethodArgumentNotValidExceptionDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
@@ -35,6 +40,23 @@ public class RestExceptionHandler {
                 "Illegal Argument"
         );
         return new ResponseEntity<>(illegalArgument,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MethodArgumentNotValidExceptionDetails> methodArgumentException(MethodArgumentNotValidException methodArgumentNotValidException){
+
+        List<FieldError> fieldErrorList = methodArgumentNotValidException.getBindingResult().getFieldErrors();
+        String fieldMessage = fieldErrorList.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining());
+        String fieldError = fieldErrorList.stream().map(FieldError::getField).collect(Collectors.joining());
+
+        MethodArgumentNotValidExceptionDetails methodArgumentException = new MethodArgumentNotValidExceptionDetails(
+                fieldMessage,
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now(),
+                "Method Argument Not Valid Exception",
+                fieldError
+        );
+        return new ResponseEntity<>(methodArgumentException,HttpStatus.BAD_REQUEST);
     }
 
 }
