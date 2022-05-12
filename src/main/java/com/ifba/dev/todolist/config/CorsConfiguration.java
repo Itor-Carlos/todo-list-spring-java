b3c391fdbb9c1c00f3a544b11c6e2cfd93f71b98
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2022-05-12 14:07:39 -0300

    CORS configuration

---
+++ src/main/java/com/ifba/dev/todolist/config/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.ifba.dev.todolist.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer{
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+
+}
